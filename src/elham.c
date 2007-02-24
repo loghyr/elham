4076ef10c5152b148ea6fb60fcadf3524ce4d155
Tom Haynes <loghyr@excfb.com>
2007-02-24 18:28:37 +0000

    Updating to get Endian test

--- src/elham.c
+++ src/elham.c
@@ -1,4 +1,36 @@
 /*
+ * Copyright (c) 2007, Thomas Haynes. All rights reserved. *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *      Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *      Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ *      Neither the name of Thomas Haynes nor the names of
+ *      any of his employers may be used to endorse or promote products
+ *      derived from this software without specific prior written
+ *      permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
  * Copyright (c) 2002-2004, Network Appliance, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -116,10 +148,6 @@ processArgs (ControlStruct *pcs, int argc, char *argv[])
 	strcpy(pcs->data.szBase, "./data");
 	strcpy(pcs->meta.szBase, "./meta");
 	strcpy(pcs->history.szBase, "./history");
-
-	strcpy(pcs->data.szBase, "/t/mana/data");
-	strcpy(pcs->meta.szBase, "/t/mana/meta");
-	strcpy(pcs->history.szBase, "/t/mana/history");
 
 	/*
 	 * Fill in dsSupplied by what is supplied on the command line.
